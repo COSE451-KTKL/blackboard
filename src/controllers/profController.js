de354db9a1250decfddab1b074475fcbc0c9a39c
Baekgyu <centneuf0109@gmail.com>
2023-10-29 12:50:12 +0900

    FTR: postNewLecture

--- src/controllers/profController.js
+++ src/controllers/profController.js
@@ -2,6 +2,12 @@ import User from "../models/User";
 import Lecture from "../models/Lecture";
 import Notice from "../models/Notice";
 import Quiz from "../models/Quiz";
+
+const updateLoggedInUser = async (req, user) => {
+    req.session.loggedInUser = user;
+    return;
+};
+
 export const getAllLectures = async (req, res) => {
     try {
         const loggedInUser = req.session.loggedInUser;
@@ -30,3 +36,32 @@ export const getNewLecture = async (req, res) => {
     }
 };
 
+export const postNewLecture = async (req, res) => {
+    try {
+        const { lectureName } = req.body;
+        const { loggedInUser } = req.session;
+        const { lectureIds } = loggedInUser;
+
+        const newLecture = await Lecture.create({
+            profId: loggedInUser._id,
+            stuIds: [],
+            lectureName,
+        });
+        if (!lectureIds.includes(newLecture._id)) {
+            await lectureIds.push(newLecture._id);
+        }
+
+        await User.findByIdAndUpdate(loggedInUser._id, {
+            lectureIds,
+        });
+        const newUser = await User.findById(loggedInUser._id);
+        await updateLoggedInUser(req, newUser);
+
+        return res.redirect("/prof/lecture");
+    } catch (errorMessage) {
+        return res.status(400).render("prof/newLecture.pug", {
+            pageTitle: "에러",
+            errorMessage,
+        });
+    }
+};
