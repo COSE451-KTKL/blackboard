b5da8878236e0d590e2e8931252c48b40e904dbb
Baekgyu <centneuf0109@gmail.com>
2023-10-29 12:28:06 +0900

    FTR: onlyIsLoggedIn

--- src/middleware.js
+++ src/middleware.js
@@ -3,3 +3,10 @@ export const saveSessionToLocal = (req, res, next) => {
     res.locals.loggedInUser = req.session.loggedInUser;
     next();
 };
+export const onlyIsLoggedIn = (req, res, next) => {
+    if (req.session.isLoggedIn) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};
