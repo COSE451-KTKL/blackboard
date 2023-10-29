330bf9cf5c5e7b8db7c32b6d54345e00fb341f3a
Baekgyu <centneuf0109@gmail.com>
2023-10-29 11:55:11 +0900

    FTR: getSignup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,15 @@
+import User from "../models/User";
 const loginUserToSession = (req, user) => {
     req.session.isLoggedIn = true;
     req.session.loggedInUser = user;
+};
+
+export const getSignup = (req, res) => {
+    try {
+        return res.render("signup", { pageTitle: "회원가입" });
+    } catch (errorMessage) {
+        return res
+            .status(400)
+            .render("signup", { pageTitle: "에러", errorMessage });
+    }
 };
