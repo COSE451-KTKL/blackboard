351045a998e69635100b44723f7b68212cfa65ae
Baekgyu <centneuf0109@gmail.com>
2023-10-29 12:36:37 +0900

    FTR: stuRouter.js

--- src/routers/stuRouter.js
+++ src/routers/stuRouter.js
@@ -1,6 +1,18 @@
 import express from "express";
-import { onlyIsLoggedIn, onlyIsLoggedOut } from "../middleware";
+
+import {
+    getSugang,
+    postSugang,
+    getAllLectures,
+    getOneLecture,
+    postOneQuiz,
+} from "../controllers/stuController";
+import { onlyIsLoggedIn } from "../middleware";
 
 const stuRouter = express.Router();
+
+stuRouter.route("/sugang").all(onlyIsLoggedIn).get(getSugang).post(postSugang);
+stuRouter.route("/lecture").all(onlyIsLoggedIn).get(getAllLectures);
+stuRouter.route("/lecture/:id").all(onlyIsLoggedIn).get(getOneLecture).post(postOneQuiz);
 
 export default stuRouter;
