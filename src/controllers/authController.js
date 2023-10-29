290949884f9c4dbcfe677f812ed6073928f330c5
Baekgyu <centneuf0109@gmail.com>
2023-10-29 11:55:29 +0900

    FTR: postSignup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
+
 const loginUserToSession = (req, user) => {
     req.session.isLoggedIn = true;
     req.session.loggedInUser = user;
@@ -11,5 +13,56 @@ export const getSignup = (req, res) => {
         return res
             .status(400)
             .render("signup", { pageTitle: "에러", errorMessage });
+    }
+};
+
+export const postSignup = async (req, res) => {
+    try {
+        const { id, pw, name, stuId, userType } = req.body;
+        if (userType === "professor" && stuId) {
+            stuId = "";
+        }
+
+        if (userType === "student" && !stuId) {
+            return res.render("signup", {
+                pageTitle: "회원가입",
+                errorMessage:
+                    "학생 회원은 학번을 반드시 입력해야 합니다. 다시 시도해주세요.",
+            });
+        }
+
+        const existsId = await User.exists({ id });
+        if (existsId) {
+            return res.status(400).render("signup", {
+                pageTitle: "회원가입",
+                errorMessage:
+                    "같은 아이디를 가진 계정이 이미 존재합니다. 다시 시도해주세요.",
+            });
+        }
+        const existsStuId =
+            (await User.exists({ stuId })) && userType === "student";
+        if (existsStuId) {
+            return res.status(400).render("signup", {
+                pageTitle: "회원가입",
+                errorMessage:
+                    "같은 학번을 가진 계정이 이미 존재합니다. 다시 시도해주세요.",
+            });
+        }
+
+        const user = await User.create({
+            id,
+            pw,
+            name,
+            stuId,
+            userType,
+            lectureIds: [],
+        });
+        loginUserToSession(req, user);
+        return res.redirect("/");
+    } catch (errorMessage) {
+        return res.status(400).render("signup", {
+            pageTitle: "에러",
+            errorMessage,
+        });
     }
 };
