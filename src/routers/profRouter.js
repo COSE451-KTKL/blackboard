72237583917484e8dfff3f563ae7693ad077c730
Baekgyu <centneuf0109@gmail.com>
2023-10-29 12:33:21 +0900

    FTR: profRouter.js

--- src/routers/profRouter.js
+++ src/routers/profRouter.js
@@ -1,6 +1,39 @@
 import express from "express";
-import { onlyIsLoggedIn, onlyIsLoggedOut } from "../middleware";
+import {
+    getAllLectures,
+    getNewLecture,
+    postNewLecture,
+    getOneLecture,
+    getNewNotice,
+    postOneNotice,
+    getNewQuiz,
+    postOneQuiz,
+    getAllStudents,
+} from "../controllers/profController";
+import { onlyIsLoggedIn } from "../middleware";
 
 const profRouter = express.Router();
+
+profRouter.route("/lecture").all(onlyIsLoggedIn).get(getAllLectures);
+profRouter
+    .route("/lecture/newLecture")
+    .all(onlyIsLoggedIn)
+    .get(getNewLecture)
+    .post(postNewLecture);
+profRouter
+    .route("/lecture/newNotice/:id")
+    .all(onlyIsLoggedIn)
+    .get(getNewNotice)
+    .post(postOneNotice);
+profRouter
+    .route("/lecture/newQuiz/:id")
+    .all(onlyIsLoggedIn)
+    .get(getNewQuiz)
+    .post(postOneQuiz);
+profRouter.route("/lecture/:id").all(onlyIsLoggedIn).get(getOneLecture);
+profRouter
+    .route("/student")
+    .all(onlyIsLoggedIn)
+    .get(getAllStudents)
 
 export default profRouter;
