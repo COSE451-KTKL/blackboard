e5fda62c4c4d80ac4a0cbced151dff62b5d5f567
Baekgyu <centneuf0109@gmail.com>
2023-10-29 11:20:27 +0900

    ADD: initialize NodeJS project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`✅ ${PORT}번 포트에 서버 연결됨`);
+});
+
+
