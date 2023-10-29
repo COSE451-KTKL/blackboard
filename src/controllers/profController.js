e64cc8cb67edd6353607b61e94f1314a379a4f62
Baekgyu <centneuf0109@gmail.com>
2023-10-29 12:49:12 +0900

    FTRL getAllLectures

--- src/controllers/profController.js
+++ src/controllers/profController.js
@@ -2,3 +2,19 @@ import User from "../models/User";
 import Lecture from "../models/Lecture";
 import Notice from "../models/Notice";
 import Quiz from "../models/Quiz";
+export const getAllLectures = async (req, res) => {
+    try {
+        const loggedInUser = req.session.loggedInUser;
+        const lectures = await Lecture.find({ profId: loggedInUser._id });
+        return res.render("prof/lecture.pug", {
+            pageTitle: "강의 목록",
+            lectures,
+        });
+    } catch (errorMessage) {
+        return res.status(400).render("prof/lecture.pug", {
+            pageTitle: "에러",
+            lectures: null,
+            errorMessage,
+        });
+    }
+};
