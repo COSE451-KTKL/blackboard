1cea6130b419957461678587cb3f8314d596de29
Baekgyu <centneuf0109@gmail.com>
2023-10-29 12:27:02 +0900

    FTR: connect routers to rootRouter

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
 
+import authRouter from "./authRouter";
+import stuRouter from "./stuRouter";
+import profRouter from "./profRouter";
 
 import { getHome } from "../controllers/rootController";
 
@@ -7,5 +10,8 @@ const rootRouter = express.Router();
 
 rootRouter.route("/").get(getHome);
 
+rootRouter.use("/auth", authRouter);
+rootRouter.use("/stu", stuRouter);
+rootRouter.use("/prof", profRouter);
 
 export default rootRouter;
