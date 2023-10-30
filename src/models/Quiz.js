3f7b59a55a69aa479f34d53474883358592c6dd5
Baekgyu <centneuf0109@gmail.com>
2023-10-30 16:31:00 +0900

    FIX: Quiz.js

--- src/models/Quiz.js
+++ src/models/Quiz.js
@@ -2,14 +2,13 @@ import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema(
     {
-        profId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
         lectureId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Lecture",
             required: true,
         },
-        problem: { type: String, required: true },
-        answer: { type: String, required: true },
+        quizProblem: { type: String, required: true },
+        quizAnswer: { type: String, required: true },
     },
     { timestamps: true }
 );
