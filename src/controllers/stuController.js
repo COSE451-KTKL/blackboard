7dddd34ea8473fbfe9ee493539307502348f6228
Baekgyu <centneuf0109@gmail.com>
2023-10-30 10:35:39 +0900

    FTR: updateLoggedInUser

--- src/controllers/stuController.js
+++ src/controllers/stuController.js
@@ -2,3 +2,7 @@ import Lecture from "../models/Lecture";
 import User from "../models/User";
 import Quiz from "../models/Quiz";
 
+const updateLoggedInUser = async (req, user) => {
+    req.session.loggedInUser = user;
+    return;
+};
