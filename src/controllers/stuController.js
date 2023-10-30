fe5919e8cd492c9c7bb313def3748c1da0ab49e9
Baekgyu <centneuf0109@gmail.com>
2023-10-30 10:36:06 +0900

    FTR: postSugang

--- src/controllers/stuController.js
+++ src/controllers/stuController.js
@@ -29,3 +29,36 @@ export const getSugang = async (req, res) => {
     }
 };
 
+export const postSugang = async (req, res) => {
+    try {
+        const newLectureId = req.body.lectureId;
+
+        const loggedInUser = req.session.loggedInUser;
+        const { lectureIds } = loggedInUser;
+
+        if (!lectureIds.includes(newLectureId)) {
+            await lectureIds.push(newLectureId);
+        }
+
+        await User.findByIdAndUpdate(loggedInUser._id, {
+            lectureIds,
+        });
+        const newUser = await User.findById(loggedInUser._id);
+        await updateLoggedInUser(req, newUser);
+
+        const { stuIds } = await Lecture.findById(newLectureId);
+        if (!stuIds.includes(loggedInUser._id)) {
+            await stuIds.push(loggedInUser._id);
+        }
+        await Lecture.findByIdAndUpdate(newLectureId, { stuIds });
+
+        return res.redirect("/stu/sugang");
+    } catch (errorMessage) {
+        return res.status(400).render("stu/sugang", {
+            pageTitle: "에러",
+            lectures: null,
+            errorMessage,
+        });
+    }
+};
+
