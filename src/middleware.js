b00663ef83a76319f98b8fedce97e8f237259951
newxxson <hyukjun1111@gmail.com>
2023-11-01 10:58:42 +0900

    ftr added isStu and isProf to middleware check

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,36 @@
 export const saveSessionToLocal = (req, res, next) => {
-    res.locals.isLoggedIn = Boolean(req.session.isLoggedIn);
-    res.locals.loggedInUser = req.session.loggedInUser;
-    next();
+  res.locals.isLoggedIn = Boolean(req.session.isLoggedIn);
+  res.locals.loggedInUser = req.session.loggedInUser;
+  next();
 };
 export const onlyIsLoggedIn = (req, res, next) => {
-    if (req.session.isLoggedIn) {
-        return next();
-    } else {
-        return res.redirect("/login");
-    }
+  if (req.session.isLoggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const isProf = (req, res, next) => {
+  if (req.session.loggedInUser.userType != "professor") {
+    return res.redirect("/stu/lecture");
+  } else {
+    return next();
+  }
+};
+
+export const isStu = (req, res, next) => {
+  if (req.session.loggedInUser.userType != "student") {
+    return res.redirect("/prof/lecture");
+  } else {
+    return next();
+  }
 };
 
 export const onlyIsLoggedOut = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return next();
-    } else {
-        return res.redirect("/");
-    }
+  if (!req.session.isLoggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
