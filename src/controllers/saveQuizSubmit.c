a6207f9843f7582e4b0717acc1b0269f49edb526
newxxson <hyukjun1111@gmail.com>
2023-11-04 17:00:39 +0900

    mod added description for the code

--- src/controllers/saveQuizSubmit.c
+++ src/controllers/saveQuizSubmit.c
@@ -6,6 +6,16 @@
 #define BUF_SIZE 300
 #endif
 
+/*
+======NOTICE======
+THIS C FILE IS THE ORIGINAL FILE OF THE saveQuizSubmit.exe
+THIS C FILE IS COMPILED WITHOUT THE COUNTERMEASURES FOR BUFFER OVERFLOW
+THIS C FILE ALSO HAS -g FLAG FOR HELPING DEBUGGING
+SHOULD YOU WISH TO RECREATE THE EXE FILE RUN
+gcc -fno-stack-protector -D_FORTIFY_SOURCE=0 -no-pie -z norelro -g saveQuizSubmit.c -o saveQuizSubmit.exe
+ALSO YOU SHOULD SET THE ASLR DISABLED.
+*/
+
 int load_to_lecture(char *submit_file_name, char *submit_text)
 {
     char buffer[BUF_SIZE];
