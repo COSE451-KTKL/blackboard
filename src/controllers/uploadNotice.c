2eba85f8750a0cdd10527bfdc996d2bdda64d2e1
newxxson <hyukjun1111@gmail.com>
2023-11-04 17:10:21 +0900

    fix fixed undefined variable

--- src/controllers/uploadNotice.c
+++ src/controllers/uploadNotice.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 int upload_notice(char *submit_file_name, char *buffer)
 {
+    char file_directory[100];
     snprintf(file_directory, sizeof(file_directory), "./uploads/%s", submit_file_name);
 
     FILE *file = fopen(file_directory, "w");
