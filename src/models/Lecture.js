973749301b827d1c1ced96c7facecbadfacb4a8d
newxxson <hyukjun1111@gmail.com>
2023-11-05 20:05:56 +0900

    mod modified vulnerabilities for db

--- src/models/Lecture.js
+++ src/models/Lecture.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
 const lectureSchema = new mongoose.Schema(
-    {
-        profId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        stuIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-        lectureName: { type: String, required: true },
-        noticeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Notice" }],
-        quizId: {type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
+  {
+    profId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    stuIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    lectureName: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9\s\-_]+$/.test(v); // Regex to validate lecture name
+        },
+        message: (props) => `${props.value} is not a valid lecture name!`,
+      },
     },
-    { timestamps: true }
+    noticeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Notice" }],
+    quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
+  },
+  { timestamps: true }
 );
 
 const Lecture = mongoose.model("Lecture", lectureSchema);
