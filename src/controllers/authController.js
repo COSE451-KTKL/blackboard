b8221892a283ac5fdb8637254c010e70134fd70a
newxxson <hyukjun1111@gmail.com>
2023-11-07 16:32:57 +0900

    mod modified postLogin function to use c decoder function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import path from "path";
+import { spawn } from "child_process";
 
 const loginUserToSession = async (req, user) => {
   req.session.isLoggedIn = true;
@@ -79,15 +81,53 @@ export const getLogin = (req, res) => {
 
 export const postLogin = async (req, res) => {
   try {
-    const { id, pw } = req.body;
-    const user = await User.findOne({ id });
+    const { id, pw, encryptedId, encryptedPw } = req.body; // get Id and Pw from url query
+    console.log(id, pw, encryptedId, encryptedPw);
+    if (!encryptedId || !encryptedPw) {
+      return res.status(400).render("login", {
+        pageTitle: "로그인",
+        errorMessage:
+          "ID와 비밀번호를 모두 제공해야 합니다. 다시 시도해주세요.",
+      });
+    }
+
+    const decryption = new Promise((resolve, reject) => {
+      // give C file the encoded id&pw
+      const cfileDirectory = path.join("src", "controllers", "login.exe"); // C file directory
+      const child = spawn(cfileDirectory, [encryptedId, encryptedPw]);
+
+      let cFileOutput = "";
+
+      child.stdout.on("data", (data) => {
+        cFileOutput += data.toString();
+      });
+
+      child.on("close", (code) => {
+        if (code === 0) {
+          // C file succeed and handle cFileOutput here
+          resolve(cFileOutput);
+        } else {
+          reject(new Error("C program went wrong"));
+        }
+      });
+    });
+
+    const cFileOutput = await decryption;
+    const [decryptedId, decryptedPw] = cFileOutput.split(" ");
+
+    console.log(decryptedPw);
+    if (/\s/.test(decryptedPw)) console.log("The string contains whitespace.");
+
+    const user = await User.findOne({ id: decryptedId });
     if (!user) {
       return res.status(400).render("login", {
         pageTitle: "로그인",
         errorMessage: "계정이 존재하지 않습니다. 다시 시도해주세요.",
       });
     }
-    const ok = await bcrypt.compare(pw, user.pw);
+
+    const ok = await bcrypt.compare(decryptedPw, user.pw);
+    console.log(ok);
     if (!ok) {
       return res.status(400).render("login", {
         pageTitle: "로그인",
@@ -95,6 +135,7 @@ export const postLogin = async (req, res) => {
       });
     }
     await loginUserToSession(req, user);
+
     return res.redirect("/");
   } catch (errorMessage) {
     return res.status(400).render("login", {
@@ -105,13 +146,14 @@ export const postLogin = async (req, res) => {
 };
 
 export const getLogout = async (req, res) => {
-  try {
-    await req.session.destroy();
-    return res.redirect("/");
-  } catch (errorMessage) {
-    return res.status(400).render("home", {
-      pageTitle: "에러",
-      errorMessage,
-    });
-  }
+  await req.session.destroy((err) => {
+    if (err) {
+      return res.status(400).render("home", {
+        pageTitle: "에러",
+        errorMessage,
+      });
+    } else {
+      return res.redirect("/");
+    }
+  });
 };
