2798179e1864993711b11d3ace357cd8ccc5a7aa
newxxson <hyukjun1111@gmail.com>
2023-11-07 16:32:32 +0900

    changed name from Login->login

--- src/controllers/Login.c
+++ src/controllers/Login.c
@@ -1,38 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/aes.h>
-
 #ifndef BUF_SIZE
-#define BUF_SIZE 20
+#define BUF_SIZE 100
 #endif
 
-// decrypt key
-const char *secret_key = "0123456789abcdef0123456789abcdef";
+// Function to shift characters back by one position
+void shift_char_back(char *str)
+{
+    if (str == NULL)
+        return;
 
+    while (*str != '\0')
+    {
+        if ((*str >= 'b' && *str <= 'z') || (*str >= 'B' && *str <= 'Z'))
+        {
+            *str = *str - 1;
+        }
+        else if (*str == 'a')
+        {
+            *str = 'z';
+        }
+        else if (*str == 'A')
+        {
+            *str = 'Z';
+        }
+        str++;
+    }
+}
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s <encrypted_id> <encrypted_password>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <encrypted_id> <encrypted_password>", argv[0]);
         return 1;
     }
 
-    // get encoded id&pw
-    const char *encrypted_id = argv[1];
-    const char *encrypted_password = argv[2];
+    // get encoded id & pw
+    char *encrypted_id = argv[1];
+    char *encrypted_password = argv[2];
+    char decrypted_id[BUF_SIZE], decrypted_password[BUF_SIZE];
 
-    // AES decrypt
-    AES_KEY aes_key;
-    AES_set_decrypt_key((const unsigned char *)secret_key, 128, &aes_key);
+    // Ensure the strings do not exceed buffer size
+    strcpy(decrypted_id, encrypted_id);
+    strcpy(decrypted_password, encrypted_password);
 
-    // buff overflow problem here
-    unsigned char decrypted_id[BUF_SIZE];
-    unsigned char decrypted_password[BUF_SIZE];
+    // Null-terminate the strings
+    decrypted_id[BUF_SIZE - 1] = '\0';
+    decrypted_password[BUF_SIZE - 1] = '\0';
 
-    AES_decrypt((const unsigned char *)encrypted_id, decrypted_id, &aes_key);
-    AES_decrypt((const unsigned char *)encrypted_password, decrypted_password, &aes_key);
+    // Shift the characters back to their original values
+    shift_char_back(decrypted_id);
+    shift_char_back(decrypted_password);
 
-    printf("%s %s\n", decrypted_id, decrypted_password);
+    printf("%s %s", decrypted_id, decrypted_password);
     return 0;
 }
