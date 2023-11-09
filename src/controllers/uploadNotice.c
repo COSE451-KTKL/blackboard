acb2faf83ec90ace8d432a49ef525853590e3dc4
newxxson <hyukjun1111@gmail.com>
2023-11-10 00:22:30 +0900

    changed dir to abs dir

--- src/controllers/uploadNotice.c
+++ src/controllers/uploadNotice.c
@@ -4,7 +4,7 @@
 int upload_notice(char *submit_file_name, char *buffer)
 {
     char file_directory[100];
-    snprintf(file_directory, sizeof(file_directory), "./uploads/lectures/%s", submit_file_name);
+    snprintf(file_directory, sizeof(file_directory), "/docker/uploads/lectures/%s", submit_file_name);
 
     FILE *file = fopen(file_directory, "w");
     fprintf(file, buffer);
