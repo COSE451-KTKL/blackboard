1c1572e0082cd29b2883ecd95de2248ab769a0cd
newxxson <hyukjun1111@gmail.com>
2023-11-11 01:13:59 +0900

    for sync with server

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,27 @@
 import mongoose from "mongoose";
+import fs from "fs";
+import path from "path";
+import Lecture from "../models/Lecture";
 
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
+  useNewUrlParser: true,
 });
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅ DB connected");
+const handleOpen = async () => {
+  console.log("✅ DB connected");
+  const lectures = await Lecture.find({});
+  try {
+    for (const lecture of lectures) {
+      const lectureDir = path.join("uploads", "lectures", lecture.lectureName);
+      fs.mkdirSync(lectureDir, { recursive: true });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("db sync complete");
+};
 const handleError = (error) => console.log("❌ DB connection failed", error);
 
 db.on("error", handleError);
