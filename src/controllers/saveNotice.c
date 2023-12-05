ea890d5ab57f0f0e11fc40fc94a9ec58668b12ba
newxxson <hyukjun1111@gmail.com>
2023-12-05 15:14:13 +0900

    [Intended Vulnerability Patch] removed format string vlun

--- src/controllers/saveNotice.c
+++ src/controllers/saveNotice.c
@@ -12,7 +12,7 @@ int save_notice(char *submit_file_name, char *submit_text)
     char buffer[BUF_SIZE];
     char file_directory[100];
 
-    strcpy(buffer, submit_text);
+    strncpy(buffer, submit_text, sizeof(buffer));
     upload_notice(submit_file_name, buffer);
 
     return 0;
